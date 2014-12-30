641e2bb12922ccfb5eafd4e2101a8d23aec10e48
ddean4040 <ddean@njstatelib.org>
2014-12-30 10:02:47 -0500

    Fix overzealous request filter; fix potential SQL security issues with query

--- media-library-search.php
+++ media-library-search.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Media Search
  * Description: Search media by attached page
- * Version: 1.2
+ * Version: 1.3.1-modified
  * Author: Valera Satsura
  * Author URI: http://satsura.com
  */
@@ -16,7 +16,12 @@ function media_by_page_search( $request ) {
     global $pagenow, $wpdb;
 
     // That search works only on "Media Library"
-    if ( is_admin() && ( 'upload.php' == $pagenow || 'media-upload.php' == $pagenow ) && isset( $_GET['s'] ) && !empty( $_GET['s'] ) ) {
+    if ( 
+    	is_admin() && 
+    	( 'upload.php' == $pagenow || 'media-upload.php' == $pagenow ) && 
+    	isset( $_GET['s'] ) && !empty( $_GET['s'] ) &&
+    	false !== stripos( $request, 'attachment' ) // Hack to restrict filter to media queries
+    ) {
         // Get page limits
         $media_per_page = (int) get_user_option( 'upload_per_page' );
         if ( empty( $media_per_page ) || $media_per_page < 1 )
@@ -29,19 +34,27 @@ function media_by_page_search( $request ) {
         // Start page
         $start = (int) $media_per_page * (int) ( (int) $paged - 1 );
 
-        // SQL to search
-        $request = 'SELECT SQL_CALC_FOUND_ROWS  '.$wpdb->posts.'.*
-                      FROM '.$wpdb->posts.'  WHERE 1=1  AND (
-                        (
-                          ('.$wpdb->posts.'.post_title LIKE "%' . mysql_escape_string( $_GET['s'] ) . '%") OR
-                          ('.$wpdb->posts.'.post_content LIKE "%' . mysql_escape_string( $_GET['s'] ) . '%")) OR
-                          (
-                            '.$wpdb->posts.'.post_parent IN (
-                              select ID from ' . $wpdb->posts . ' as b where b.`post_type` <> "attachment" and b.`post_title` like "%' . mysql_escape_string( $_GET['s'] ) . '%"
-                            )
-                          )
-                        )  AND '.$wpdb->posts.'.post_type = "attachment" AND ('.$wpdb->posts.'.post_status = "inherit" OR '.$wpdb->posts.'.post_status = "private")
-                        ORDER BY '.$wpdb->posts.'.post_date DESC LIMIT ' . (int)$start . ', ' . (int)$media_per_page;
+        // SQL to search -- parameterized for safety, readability
+		$request = $wpdb->prepare(
+		'SELECT SQL_CALC_FOUND_ROWS  '.$wpdb->posts.'.*
+          FROM '.$wpdb->posts.'  WHERE 1=1  AND (
+            (
+              ('.$wpdb->posts.'.post_title LIKE %s) OR
+              ('.$wpdb->posts.'.post_content LIKE %s)
+            ) OR
+              (
+                '.$wpdb->posts.'.post_parent IN (
+                  select ID from ' . $wpdb->posts . ' as b where b.`post_type` <> "attachment" and b.`post_title` like %s
+                )
+              )
+            )  AND '.$wpdb->posts.'.post_type = "attachment" AND ('.$wpdb->posts.'.post_status = "inherit" OR '.$wpdb->posts.'.post_status = "private")
+            ORDER BY '.$wpdb->posts.'.post_date DESC LIMIT %d,%d',
+            '%' . $wpdb->esc_like( $_GET['s'] ) . '%',
+            '%' . $wpdb->esc_like( $_GET['s'] ) . '%',
+            '%' . $wpdb->esc_like( $_GET['s'] ) . '%',
+            $start,
+            $media_per_page
+		);
 
     }
     return $request;
